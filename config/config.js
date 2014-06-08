31a2805f9952af83ebbbb4e3da2ececa5e84bda9
Steven Zeiler <zeiler.steven@gmail.com>
2014-06-08 05:30:22 +0000

    [CHORE] Add package.json, config.

---
+++ config/config.js
@@ -0,0 +1,14 @@
+var config = require('nconf');
+
+config
+  .file({ file: __dirname+'/config.json' })
+  .env();
+
+config.defaults({
+  'GATEWAY_URL': '',
+  'GATEWAY_API_KEY': '',
+  'TOTAL_RECEIVED': 0
+});
+
+module.exports = config;
+
